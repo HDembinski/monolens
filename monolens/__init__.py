6237167cbaa704660e996e1534526788b51dfa56
Hans Dembinski <hans.dembinski@gmail.com>
2021-05-28 19:35:25 +0200

    support ctrl+c

--- monolens/__init__.py
+++ monolens/__init__.py
@@ -5,7 +5,9 @@ def main():
     from PySide6.QtWidgets import QApplication
     from .widget import Widget
     import sys
+    import signal
 
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
     app = QApplication(sys.argv)
     image = Widget()
     image.show()
