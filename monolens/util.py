a02ce55e6b811aa225dcd3d48875f981729f9cca
Hans Dembinski <hans.dembinski@gmail.com>
2021-05-30 21:51:05 +0200

    wip

--- monolens/util.py
+++ monolens/util.py
@@ -2,7 +2,8 @@ import os
 import sys
 from PySide6 import QtGui
 import numpy as np
-import numba as nb
+
+# import numba as nb
 
 DEBUG = int(os.environ.get("DEBUG", "0"))
 
@@ -48,7 +49,7 @@ def qimage_array_view(image):
     return raw.view(dtype, np.recarray)
 
 
-@nb.njit
+# @nb.njit
 def _grayscale(d, s):
     g = 0.299 * s.r + 0.587 * s.g + 0.114 * s.b
     d.r = g
@@ -63,28 +64,42 @@ def grayscale(dest, source):
     _grayscale(d, s)
 
 
-def to_lms(a):
-    out = np.empty((3,) + a.shape, np.float16)
-    out[0] = 0.3904725 * a.r + 0.54990437 * a.g + 0.00890159 * a.b
-    out[1] = 0.07092586 * a.r + 0.96310739 * a.g + 0.00135809 * a.b
-    out[2] = 0.02314268 * a.r + 0.12801221 * a.g + 0.93605194 * a.b
-    return out
+cb = np.array(
+    [
+        [[1, 0, 0], [1.10104433, 0, -0.00901975], [0, 0, 1]],
+        [[0, 0.90822864, 0.008192], [0, 1, 0], [0, 0, 1]],
+        [[1, 0, 0], [0, 1, 0], [-0.15773032, 1.19465634, 0]],
+    ],
+    dtype=np.float16,
+)
+rgb2lms = np.array(
+    [
+        [0.3904725, 0.54990437, 0.00890159],
+        [0.07092586, 0.96310739, 0.00135809],
+        [0.02314268, 0.12801221, 0.93605194],
+    ],
+    dtype=np.float16,
+)
+lms2rgb = np.linalg.inv(rgb2lms)
 
-
-def from_lms(rgb, a):
-    rgb.r = np.einsum("i,...i", [2.85831110e00, -1.62870796e00, -2.48186967e-02], a)
-    rgb.g = np.einsum("i,...i", [-2.10434776e-01, 1.15841493e00, 3.20463334e-04], a)
-    rgb.b = np.einsum("i,...i", [-4.18895045e-02, -1.18154333e-01, 1.06888657e00], a)
+cb_full = [np.linalg.multi_dot(lms2rgb, cbi, rgb2lms) for cbi in cb]
 
 
 def colorblindness(dest, source, type):
     s = qimage_array_view(source)
     d = qimage_array_view(dest)
-    lms = to_lms(s)
-    if type == 0:  # protanopia
-        lms[0] = 0.90822864 * lms[1] + 0.008192 * lms[2]
-    elif type == 1:  # deuteranopia
-        lms[1] = 1.10104433 * lms[0] - 0.00901975 * lms[2]
-    elif type == 2:  # tritanopia
-        lms[2] = -0.15773032 * lms[0] + 1.19465634 * lms[1]
-    from_lms(d, lms)
+    d.r = (
+        cb_full[type][0, 0] * s.r
+        + cb_full[type][0, 1] * s.g
+        + cb_full[type][0, 2] * s.b
+    )
+    d.g = (
+        cb_full[type][1, 0] * s.r
+        + cb_full[type][1, 1] * s.g
+        + cb_full[type][1, 2] * s.b
+    )
+    d.b = (
+        cb_full[type][2, 0] * s.r
+        + cb_full[type][2, 1] * s.g
+        + cb_full[type][2, 2] * s.b
+    )
